d992925c08c67dd8fc6e8a003e4eeb00532b3274
Grass-ias <ferdyprasetyaputra08@gmail.com>
2024-11-13 14:58:44 +0700

    Update ListFunction.py

--- Tugas Laprak 9-11-2024/ListFunction.py
+++ Tugas Laprak 9-11-2024/ListFunction.py
@@ -99,7 +99,7 @@ def ElmtkeN(N,L):
 
 # print(ElmtkeN(2, [3,4,5]))
 
-# IsMember: elemen, List -> boolean
+# IsMember: elemen/list, List -> boolean
 # IsMember(X,L) adalah benar jika X adalah elemen list L
 def IsMember(x, L):
     if IsEmpty(L):
@@ -110,7 +110,7 @@ def IsMember(x, L):
         else:
             return (IsMember(x, Head(L)))
 
-# print(IsMember(2, [3, 4, 6, 2]))
+# print(IsMember([3], [[6,7], 8, 3, 9, [2], [1,2,3]]))
 
 # Copy: List -> List
 # Copy(L) : Menghasilkan list yang identik dengan list asal
@@ -143,6 +143,7 @@ def Konkat(L1, L2):
 
 # print(Konkat([10, 14, 60, 100], [35, 66, 90]))
 
+
 # SumElmt: List of integer -> integer
 # SumElmt(L) menghasilkan jumlahan dari setiap elemen di list L
 def SumElmt(L):
@@ -150,15 +151,14 @@ def SumElmt(L):
         return 0
     else:
         return FirstElmt(L) + SumElmt(Tail(L))
-
 # print(SumElmt([1, 2, 3, 4, 5]))
 
 # AvgElmt: List of integer -> integer
 # AvgElmt(L) menghasilkan nilai rata-rata
 def AvgElmt(L):
     return int(SumElmt(L)/NbElmt(L))
-
 # print(AvgElmt([1, 2, 3, 4, 5]))
+
 
 # MaxElmt: List of integer -> integer
 # MaxElmt(L) mengembalikan elemen maksimun dari list L
@@ -170,19 +170,18 @@ def MaxElmt(L):
         return LastElmt(L)
     else:
         return max2(FirstElmt(L), MaxElmt(Tail(L)))
-
 # print(MaxElmt([1, 2, 10, 2, 3]))
+
 
 # MaxNB: List of integer -> <integer, integer>
 # MaxNB(L) menghasilkan <max,countMax>
-#   dengan max adalah elemen maksimun list L
-#   dan countMax adalah banyaknya kemunculan max di list L
+#   Dengan max adalah elemen maksimun list L
+#   Dan countMax adalah banyaknya kemunculan max di list L
 def maxlist(x):
     if IsOneElmt(x):
         return LastElmt(x)
     else:
         return max2(LastElmt(x), maxlist(Head(x)))
-
 def NbOcc(x, L):
     if IsOneElmt(L):
         if x == FirstElmt(L):
@@ -194,27 +193,26 @@ def NbOcc(x, L):
             return 1 + NbOcc(x, Tail(L))
         else:
             return NbOcc(x, Tail(L))
-
+        
 def MaxNb(L):
     return [maxlist(L), NbOcc(maxlist(L), L)]
-
 # print(MaxNb([100, 100000, 3000000, 30000000, 30000000, 1000000000]))
+
 
 # AddList: 2 List of integer -> List of integer
 # AddList(L1,L2) menghasilkan list baru yang setiap elemennya
-#   adalah hasil penjumlahan setiap elemen di L1 dan L2 pada posisi yang sama
+#   Adalah hasil penjumlahan setiap elemen di L1 dan L2 pada posisi yang sama
 def AddList(L1, L2):
     if IsEmpty(L1) or IsEmpty(L2):
         return []
     else:
         return Konso((FirstElmt(L1) + FirstElmt(L2)), AddList(Tail(L1), Tail(L2)))
-
 # print(AddList([1, 6, 10], [3, 9, 6]))
+
 
 # IsPalindrom: List of character -> boolean
 # IsPalindrom(L) benar jika L merupakan kata palindrom
-#   yaitu kata yang sama jika dibaca dari kiri atau kanan
-#   contoh: RUSAK, KASUR RUSAK, NABABAN
+#   Yaitu kata yang sama jika dibaca dari kiri atau kanan
 def IsPalindrom(L):
     if IsEmpty(L):
         return True
@@ -222,8 +220,8 @@ def IsPalindrom(L):
         return True
     else:
         return FirstElmt(L) == LastElmt(L) and IsPalindrom(Head(Tail(L)))
+# print(IsPalindrom(['S', 'U', 'S']))
 
-# print(IsPalindrom(['K', 'A', 'T', 'A', 'K']))
 
 # 6 November 2024
 # DEFINISI DAN SPESIFIKASI OPERASI YANG DIBUTUHKAN UNTUK HIMPUNAN
@@ -240,7 +238,8 @@ def Rember(e,L):
             return Tail(L)
         else:
             return Konso(FirstElmt(L), Rember(Tail(L)))
-        
+# print(Rember(1,[1,2]))
+
 def MultiRember(e,L):
     if IsEmpty(L):
         return []
@@ -248,8 +247,7 @@ def MultiRember(e,L):
         if FirstElmt(L) == e:
             return MultiRember(e,Tail(L))
         else:
-            return Konso(FirstElmt(L), MultiRember(e,Tail(L)))
-        
+            return Konso(FirstElmt(L), MultiRember(e,Tail(L)))  
 # print(MultiRember(2,[2,3,4,1,2]))
 # print(MultiRember(4,[2,3,4,1,4,4]))
 
@@ -262,9 +260,9 @@ def IsSet(L):
             return False
         else:
             return IsSet(Tail(L))
-
 # print(IsSet([1,2,3,4,5]))
 # print(IsSet([1,2,3,4,5,5]))
+
 
 def MakeSet1(L):
     if IsEmpty(L):
@@ -274,25 +272,26 @@ def MakeSet1(L):
             return MakeSet1(Tail(L))
         else:
             return Konso(FirstElmt(L), MakeSet1(Tail(L)))
-        
 # print(MakeSet1([1,2,2,3,4,5,5]))
+
 
 def MakeSet2(L):
     if IsEmpty(L):
         return []
     else:
         return Konso(FirstElmt(L), MakeSet2(MultiRember(FirstElmt(L), Tail(L))))
-
 # print(MakeSet2([1,2,2,3,4,4,4,5,5]))
+
 
 def KonsoSet(e,L):
     if IsEmpty(L):
         return [e]+L
     else:
         if IsMember(e,L):
-            return 
-        
-
+            return L
+        else:
+            return [e]+L
+# print(KonsoSet(1,[2,3,4]))
 
 
 def IsSubSet(L1,L2):
@@ -303,19 +302,19 @@ def IsSubSet(L1,L2):
             return IsSubSet(Tail(L1),L2)
         else:
             return False
-
 # print(IsSubSet([],[1]))
 # print(IsSubSet([1,2],[1,2]))
 # print(IsSubSet([0,1],[1,2]))
 
+
 def IsEqSet1(L1,L2):
     return IsSubSet(L1,L2) and IsSubSet(L2,L1)
-
 # print(IsEqSet1([1],[1]))
 # print(IsEqSet1([1,2],[1]))
 # print(IsEqSet1([],[]))
 # print(IsEqSet1([],[1]))
     
+
 def IsEqSet2(L1,L2):
     if IsEmpty(L1) and IsEmpty(L2):
         return True
@@ -324,20 +323,12 @@ def IsEqSet2(L1,L2):
             return False
         else:
             return IsEqSet2(Tail(L1),Tail(L2))
-    
 # print(IsEqSet2([1],[1]))
 # print(IsEqSet2([1,2],[1]))
 # print(IsEqSet2([],[]))
 # print(IsEqSet2([],[1]))
 
 
-
-
-
-
-
-
-            
 
 
 
